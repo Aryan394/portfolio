75144167251f7016babc4250ea1637b028355b9c
Aryan Singh <80955630+Aryan394@users.noreply.github.com>
2022-10-15 00:19:21 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ const obeserver = new IntersectionObserver((entries) =>{
         if( entry.isIntersecting){
             entry.target.classList.add('show');
         }
-        else{
-            entry.target.classList.remove('show');
-        }
+//         else{
+//             entry.target.classList.remove('show');
+//         }
     });
 });
 const hiddenElements = document.querySelectorAll('.hidden');
